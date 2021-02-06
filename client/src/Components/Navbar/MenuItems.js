39b733bb3334c5e938c7335c7fd349c8278c40cb
Jeremy Guillemet <guillemet.jeremy087@gmail.com>
2021-02-06 23:02:39 +0100

    :sparkles: animation de l'icon de la nav + nav principal

---
+++ client/src/Components/Navbar/MenuItems.js
@@ -0,0 +1,46 @@
+import {FaHome, FaTwitch, FaYoutube} from "react-icons/fa";
+
+const iTwitter = "i-twitter.png"
+const iInstagram = "i-instagram.png"
+const iTwitch = "i-twitch.png"
+const iYoutube = "i-youtube.png"
+export const MenuItems = [
+    {
+        label: "Accueil",
+        url: "/",
+        icons: <FaHome size={22}/>
+    },
+    {
+        label: "Twitch",
+        url: "/twitch-stream",
+        icons: <FaTwitch size={22}/>
+    },
+    {
+        label: "Youtube",
+        url: "/youtube-videos",
+        icons: <FaYoutube size={22}/>
+    }
+]
+
+export const SocialNetworks = [
+    {
+        label: "Twitter-01",
+        url: "", //TODO: mettre le lien
+        icons: iTwitter
+    },
+    {
+        label: "Instagram",
+        url: "", //TODO: mettre le lien
+        icons: iInstagram
+    },
+    {
+        label: "Twitch-02",
+        url: "", //TODO: mettre le lien
+        icons: iTwitch
+    },
+    {
+        label: "Youtube-03",
+        url: "", //TODO: mettre le lien
+        icons: iYoutube
+    }
+]
---
+++ client/src/Components/Navbar/Navbar.jsx
@@ -0,0 +1,52 @@
+import React, {useState} from 'react';
+import {FaBars, FaTimes} from "react-icons/fa";
+import {MenuItems, SocialNetworks} from "./MenuItems";
+import {NavLink} from "react-router-dom";
+
+const Navbar = () => {
+    const [menuOpen, setMenuOpen] = useState(true);
+    return <>
+        <div className={`overlay-navbar ${menuOpen ? "overlay-active" : "nav-disable"}`}
+             onClick={() => setMenuOpen(!menuOpen)}/>
+        <div className="nav-container">
+            <div className="nav-logo">
+                <img src="/assets/navbar/Logo.png" alt="logo"/>
+            </div>
+            <button className={`btn-humberger ${menuOpen ? "btn-humberger_active" : ""}`}
+                    onClick={() => setMenuOpen(!menuOpen)}>
+                {!menuOpen ? <FaBars size={30}/> : <FaTimes size={30}/>}
+            </button>
+            <div className={`nav-primary ${menuOpen ? "nav-open" : ""}`}>
+                <nav>
+                    <div className="nav-open_logo">
+                        <img src="/assets/navbar/Logo.png" alt="logo officiel de the_m3ry" height="150" width="150"/>
+
+                    </div>
+                    <ul className="nav-list">
+                        {MenuItems.map((link) => <>
+                            <li key={link.label}>
+                                <NavLink to={link.url} className="nav-item">{link.icons}&nbsp;{link.label}</NavLink>
+                            </li>
+                        </>)}
+                    </ul>
+                </nav>
+
+                <div className="nav-footer">
+                    <h3>Retrouvez moi sur mes réseaux</h3>
+                    <ul>
+                        {SocialNetworks.map(socialLink => <>
+                            <li key={socialLink.label} className="social-network-item">
+                                <a href={socialLink.url}>
+                                    <img src={`/assets/icons/${socialLink.icons}`} alt={socialLink.label}/>
+                                </a>
+                            </li>
+                        </>)}
+                    </ul>
+                </div>
+
+            </div>
+        </div>
+    </>
+};
+
+export default Navbar;
