1360fee665e3f08affa1571abe32ab4b8f11b50d
Jeremy Guillemet <guillemet.jeremy087@gmail.com>
2021-02-10 20:08:11 +0100

    :sparkles: Ajout du context de twitch & youtube

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,16 @@
 import {useState, useEffect} from "react"
 import Routes from "./Components/Routes/Routes";
 import {ThemeContext} from "./context/ThemeContext";
-
+import axios from "axios";
+import {YoutubeContext} from "./context/YoutubeContext";
+import {TwitchContext} from "./context/TwitchContext";
+import {useDataYoutube} from "./Hooks/useDataYoutube";
 function App() {
+  /*
+  * Gestion du theme
+  * ============================
+  * ============================
+  * */
   const [themeState, setThemeState] = useState({isDark: false, loading: true})
   const matchMedia = window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches
   const getPreferenceTheme = localStorage.getItem("Mery_Theme")
@@ -19,9 +27,54 @@ function App() {
       setThemeState({isDark: themePref.isDark, loading: false})
     }
   }, [])
+
+  /*
+  * Gestion des contextes Youtube & Twitch
+  * ============================
+  * ============================
+  * */
+
+  /*Youtube*/
+  const youtubeState = useDataYoutube()
+  /*-- Fin youtube --*/
+
+  /*Twitch */
+  const [twitchState, setTwitchState] = useState({
+    loading: true,
+    isOnLive: false,
+    titleStream: "",
+    viewers: ""
+  })
+  useEffect(() => {
+    const getInfoStream = async () => {
+      await axios.get('http://localhost:8000/twitch')
+          .then((res) => {
+            setTwitchState({
+              ...twitchState,
+              loading: false,
+              isOnLive: res.data.type === "live" && true,
+              titleStream: res.data.title,
+              viewers: res.data.viewer_count
+            })
+          })
+          .catch(err => {
+            console.log(err)
+            setTwitchState({
+              ...twitchState,
+              loading: false
+            })
+          })
+    }
+    return getInfoStream()
+  }, [])
+
   return <>
     <ThemeContext.Provider value={themeState}>
-      <Routes/>
+      <TwitchContext.Provider value={twitchState}>
+        <YoutubeContext.Provider value={youtubeState}>
+          <Routes/>
+        </YoutubeContext.Provider>
+      </TwitchContext.Provider>
     </ThemeContext.Provider>
   </>
 }
