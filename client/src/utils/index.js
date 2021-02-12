c3f4bb6948f7ad92cf70b7158617721eb1336637
Jeremy Guillemet <guillemet.jeremy087@gmail.com>
2021-02-12 16:23:30 +0100

    :sparkles: ajout de tools pour récupérer / mettre dans le localstorage, tools pour calculer la différence entre 2 date

--- client/src/utils/index.js
+++ client/src/utils/index.js
@@ -14,18 +14,38 @@ export const isEmpty = (value) => {
  * @param d2
  * @returns {string}
  */
-export function dayDiff(d1, d2)
-{
+export function dayDiff(d1, d2) {
     d1 = d1 / 86400000;
     d2 = d2 / 86400000;
     return Number(d2 - d1).toFixed(2);
 }
 
-export function getLocalStorageParsed (key){
-    const storedData = localStorage.getItem(key)
-    return JSON.parse(storedData)
+export function getLocalStorage(key) {
+    return localStorage.getItem(key)
 }
 
-export async function setLocalStorageParsed (key, data){
-    return localStorage.setItem(key, await JSON.stringify(data))
+export function getLocalStorageParsed(key) {
+    if (getLocalStorage(key) === null || undefined)
+        return null
+    else
+        return JSON.parse(getLocalStorage(key))
+}
+
+
+export function checkLocalStorage(key, numberOfDay, callback, data) {
+    const cacheLocal = getLocalStorageParsed(key)
+    if (cacheLocal === null) {
+        console.log("retour de la callback pas de cache")
+        return callback(() => {
+            setLocalStorageParsed(key, data)
+        })
+    }else {
+        if (cacheLocal && dayDiff(cacheLocal.dateStorage, Date.now()) <= numberOfDay)
+            return cacheLocal
+    }
+
+}
+
+export  function setLocalStorageParsed(key, data) {
+    return localStorage.setItem(key, JSON.stringify(data))
 }
